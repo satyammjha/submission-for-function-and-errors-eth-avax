090cea90b137bbf4520d9595c4a67fc8bf2b28c3
Satyam Jha <satyammjha0@gmail.com>
2024-06-16 16:30:04 +0530

    Update Area.sol

--- Area.sol
+++ Area.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
 
+contract RectangleAreaCalculator {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function calculateArea(uint256 length, uint256 width) public view returns (uint256) {
+        require( msg.sender == owner, "Sorry stranger, Only owners have rights to calculate the area" );
+      
+        assert(length > 0 && width > 0);
+        uint256 area = length * width;
+
+        if (area / length != width) {
+            revert( "Error: Length and Width were inputted incorrectly.");
+        }
+
+        return area;
+    }
+}
